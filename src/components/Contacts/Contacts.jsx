9d5eafa763c5254daeec3b7e6e4db3d6c8e054d3
maqweb <balikhinv@bk.ru>
2019-11-13 15:47:03 +0300

    add Contacts block

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import style from './Contacts.module.css'
 
 function Contacts() {
-  return (
-    <div className={style.contacts}>
-      Contacts
-    </div>
-  );
+    return (
+        <div className={style.contacts}>
+            <div className={style.container}>
+                <div className={style.sectionTitle}>Contacts</div>
+
+                <form className={style.form}>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <textarea/>
+                </form>
+
+                <div className={style.buttonWrap}>
+                    <button className={style.button}>Отправить</button>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default Contacts;
