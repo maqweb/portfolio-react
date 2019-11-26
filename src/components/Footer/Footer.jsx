90d809c03f4c19775c45efc3158c7ced3faf49e5
maqweb <balikhinv@bk.ru>
2019-11-26 14:26:44 +0300

    add footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,21 @@ import style from './Footer.module.css'
 function Footer() {
   return (
     <div className={style.footer}>
-      Footer
+        <div className={style.container}>
+            <div className={style.sectionTitle}>Balikhin Vitaly</div>
+
+            <div className={style.socialsWrap}>
+                <div className={style.socialItem}>github</div>
+                <div className={style.socialItem}>vk</div>
+                <div className={style.socialItem}>telegram</div>
+                <div className={style.socialItem}>linkedIn</div>
+            </div>
+
+            <div className={style.copyrights}>
+                © 2019 all rights reserved
+            </div>
+
+        </div>
     </div>
   );
 }
